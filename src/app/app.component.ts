5fbe140fb087cfca124bfd7c35b39cac44227cb6
Wendel Segadilha <wendelsegadilha99@gmail.com>
2024-06-10 22:43:01 -0300

    criando pagina login na area administrativa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Usuario } from './model/usuario';
+import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-expert';
+  tituloLogin= 'Área Administrativa';
+
+  constructor(private fb: FormBuilder, private loginService: LoginService){
+
+  }
+
+  /*Pegar dados do formulário*/
+  loginForm = this.fb.group({
+    id:[],
+    login:[null, Validators.required],
+    senha:[null, Validators.required]
+  });
+
+  /*Tranforma os dados do form em objeto*/
+  loginObjeto(): Usuario {
+    return {
+      login: this.loginForm.get("login")?.value!,
+      senha: this.loginForm.get("senha")?.value!
+    }
+  }
+
+  fazerLogin(): void {
+    const usuario = this.loginObjeto();
+    console.log(usuario);
+    this.loginService.logar(usuario);
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    urlApi: "https://jdevt3716.c34.integrator.host/lojaintegratorspring/",
+    utlApiLocal: "http://localhost:8080/lojaintegratorspring/"
+}
